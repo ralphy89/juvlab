c3da88995955878583e097c414370694bec0fdd0
Ralphy89 <43939986+ralphy89@users.noreply.github.com>
2025-05-07 12:53:28 -0400

    Can Now Register Participant / commit

--- JuvLab/bootcampManagement/urls.py
+++ JuvLab/bootcampManagement/urls.py
@@ -3,8 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.api_overview, name='home'),
-    path('add_students/', views.add_students, name='add-students'),
-    path('view_students/', views.view_students, name='view-students'),
-    path('update_students/<int:pk>/', views.update_students, name='update-students'),
+    path('add_students', views.add_students, name='add-students'),
+    path('view_students', views.view_students, name='view-students'),
+    path('update_students/<int:pk>', views.update_students, name='update-students'),
     path('change_p_status/<int:pk>', views.change_participant_status, name='change-p-status'),
 ]
--- JuvLab/static/js/form.js
+++ JuvLab/static/js/form.js
@@ -1,17 +1,37 @@
 const save_participant_btn = document.getElementById('save-student-btn');
-save_participant_btn.addEventListener('click', () => {
+save_participant_btn.addEventListener('click', async () => {
     const first_name = document.getElementById('first-name');
     const last_name = document.getElementById('last-name');
     const email = document.getElementById('student-email');
     const phone = document.getElementById('student-phone');
     const program = document.getElementById('student-program');
     const dataToSend = {
-        'first_name' : first_name.value,
-        'last_name': last_name.value,
-        'email': email.value,
-        'phone': parseInt(phone.value),
-        'program': program.value,
-    }
+            'first_name': first_name.value,
+            'last_name': last_name.value,
+            'email': email.value,
+            'phone': parseInt(phone.value),
+            'program': program.value,
+        }
+    ;
+    const response = await fetch('http://127.0.0.1:8000/api/add_students', {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(dataToSend),
+        // …
+    });
 
-    console.log(dataToSend)
+    if (response.ok) {
+        const data = await response.json();
+        location.reload();
+    }
+    document.getElementById('modal-title-h3').innerText = 'Add Participant';
 })
+
+function resetForm() {
+    // Reset all form fields
+    document.querySelectorAll('#student-form-modal input').forEach(input => {
+        input.value = '';
+    });
+}
